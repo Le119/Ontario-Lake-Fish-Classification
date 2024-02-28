9ed5b0e79d05d72117ff9a4887616b92ee10b3d1
Le119 <you@example.com>
2024-02-28 09:33:28 -0500

    Adding FC layers to take metadata input in the final layers. Performance not good.

--- ResNet.R
+++ ResNet.R
@@ -7,172 +7,138 @@ library(caret)
 
 load("processed_AnalysisData_no200.Rdata")
 
-processed_data<-processed_data_no200
+processed_data <- processed_data_no200
 
 processed_data%>%group_by(spCode,fishNum)%>%count()
 
-# Remove individuals with missing transducers
-processed_data<-processed_data%>%filter(is.na(F100)==F)
+processed_data <- processed_data %>% filter(is.na(F100) == F)
+processed_data <- processed_data %>% filter(fishNum != "LWF23018")
+processed_data <- processed_data %>% select(-F90, -F90.5)
+processed_data <- processed_data %>% filter(spCode == "81" | spCode == "316")
+processed_data$species <- ifelse(processed_data$spCode == 81, "LT", "SMB")
+processed_data <- processed_data %>% filter(is.na(aspectAngle) == F & is.na(Angle_major_axis) == F & is.na(Angle_minor_axis) == F)
+processed_data <- processed_data %>% filter(F100 > -1000)
 
-# also remove individual LWF23018 (only two pings)
-processed_data<-processed_data%>%filter(fishNum!="LWF23018")
-
-processed_data<-processed_data%>%select(-F90)
-processed_data<-processed_data%>%select(-F90.5)
-
-processed_data<-processed_data%>%filter(spCode == "81"|spCode == "316")
-
-processed_data$species<-ifelse(processed_data$spCode==81, "LT", "SMB")
-
-processed_data<-processed_data%>%filter(is.na(aspectAngle)==F & is.na(Angle_major_axis)==F)
-
-processed_data<-processed_data%>%filter(F100>-1000)
-
-#### Train/Test Set Dataset 1&2 ####
 set.seed(73)
-split<-group_initial_split(processed_data,group=fishNum,strata = species, prop=0.7)
-train<-training(split)
-val_test<-testing(split)
-split2<-group_initial_split(val_test,group=fishNum,strata = species, prop=0.5)
-validate<-training(split2)
-test<-testing(split2)
+split <- group_initial_split(processed_data, group = fishNum, strata = species, prop = 0.7)
+train <- training(split)
+val_test <- testing(split)
+split2 <- group_initial_split(val_test, group = fishNum, strata = species, prop = 0.5)
+validate <- training(split2)
+test <- testing(split2)
 
 train%>%group_by(species)%>%count()
 validate%>%group_by(species)%>%count()
 test%>%group_by(species)%>%count()
 
-train<-slice_sample(train, n = 6882, by = species)
-validate<-slice_sample(validate, n = 1125, by = species)
-test<-slice_sample(test, n = 1127, by = species)
+train <- slice_sample(train, n = 6882, by = species)
+validate <- slice_sample(validate, n = 1125, by = species)
+test <- slice_sample(test, n = 1127, by = species)
 
 train%>%group_by(species)%>%count()
 validate%>%group_by(species)%>%count()
 test%>%group_by(species)%>%count()
 
-train$y<-NA
-train$y[train$species=="LT"]<-0
-train$y[train$species=="SMB"]<-1
-summary(train$y)
-dummy_y_train<-to_categorical(train$y, num_classes = 2)
+train$y <- ifelse(train$species == "LT", 0, 1)
+dummy_y_train <- to_categorical(train$y, num_classes = 2)
+test$y <- ifelse(test$species == "LT", 0, 1)
+dummy_y_test <- to_categorical(test$y, num_classes = 2)
 
-test$y<-NA
-test$y[test$species=="LT"]<-0
-test$y[test$species=="SMB"]<-1
-summary(test$y)
-dummy_y_test<-to_categorical(test$y, num_classes = 2)
-
-x_train <- train %>% 
-  select(c(52:300))
-x_train<-x_train+10*log10(450/train$totalLength)
-x_train<-exp(x_train/10)
-x_train<-x_train%>%scale()
-x_train<-as.matrix(x_train)
+# Prepare the metadata for input
+metadata_train <- train %>% select(Angle_minor_axis, Angle_major_axis, aspectAngle) %>% as.matrix()
+metadata_test <- test %>% select(Angle_minor_axis, Angle_major_axis, aspectAngle) %>% as.matrix()
 
 xmean<-attributes(x_train)$`scaled:center`
 xsd<-attributes(x_train)$`scaled:scale`
 
-x_test <- test %>% 
-  select(c(52:300))
+x_train <- train %>% select(52:300)
+x_train<-x_train+10*log10(450/train$totalLength)
+x_train<-exp(x_train/10)
+x_train<-x_train%>%scale()
+x_train<-as.matrix(x_train)
+x_test <- test %>% select(52:300)
 x_test<-x_test+10*log10(450/test$totalLength)
 x_test<-exp(x_test/10)
 x_test<-x_test%>%scale(xmean,xsd)
 x_test<-as.matrix(x_test)
 
-## Shuffle training data
+# Shuffle training data
 set.seed(250)
-x<-sample(1:nrow(x_train))
-x_train_S= x_train[x, ] 
-dummy_y_train_S= dummy_y_train[x, ] 
+train_indices <- sample(1:nrow(x_train))
+x_train <- x_train[train_indices, ] 
+metadata_train <- metadata_train[train_indices, ]
+dummy_y_train <- dummy_y_train[train_indices, ] 
 
 set.seed(250)
-x<-sample(1:nrow(x_test))
-x_test_S= x_test[x, ] 
-dummy_y_test_S= dummy_y_test[x,] 
+test_indices <- sample(1:nrow(x_test))
+x_test <- x_test[test_indices, ] 
+metadata_test <- metadata_test[test_indices, ]
+dummy_y_test <- dummy_y_test[test_indices, ]
 
-# x_train <- array_reshape(x_train_S, c(nrow(x_train_S), 1, 249, 1))
-# x_test <- array_reshape(x_test_S, c(nrow(x_test_S), 1, 249, 1))
-input_shape <- c(249,1)
-
-
-#---------------------------------------------
+input_shape <- c(249, 1)
 
 # Define a simple residual block function
 simple_res_block <- function(input, filters, kernel_size, stride = 1) {
-  # Adjust input shape
   part <- input %>%
-    layer_conv_1d(filters = filters, kernel_size = 1, strides = stride, padding = "same") %>%
-    layer_batch_normalization()
-  
-  # First convolutional layer in the block
-  part <- part %>%
-    layer_conv_1d(filters = filters, kernel_size = kernel_size, padding = "same") %>%
+    layer_conv_1d(filters = filters, kernel_size = kernel_size, strides = stride, padding = "same") %>%
     layer_batch_normalization() %>%
-    layer_activation_relu()
-  
-  # Second convolutional layer in the block
-  part <- part %>%
-    layer_conv_1d(filters = filters, kernel_size = kernel_size, padding = "same") %>%
+    layer_conv_1d(filters = filters, kernel_size = kernel_size, strides = stride, padding = "same") %>%
+    layer_batch_normalization() %>%
+    layer_activation_relu() %>%
+    layer_conv_1d(filters = filters, kernel_size = kernel_size, strides = stride, padding = "same") %>%
     layer_batch_normalization()
-  
-  # Add the input (residual connection) to the output of the block
   output <- layer_add(list(input, part)) %>%
     layer_activation_relu()
-
   return(output)
 }
 
-# Start building the model
-input <- layer_input(shape = c(249, 1))
+# Input layers for CNN and metadata
+input_cnn <- layer_input(shape = input_shape)
+input_meta <- layer_input(shape = c(3))
 
-# Initial convolutional layer
-x <- input %>%
+# ResNet layers
+x <- input_cnn %>%
   layer_conv_1d(filters = 16, kernel_size = 3, strides = 1, padding = "same") %>%
   layer_activation_relu() %>%
-  layer_batch_normalization()
-
-# Add simple residual blocks
-x <- simple_res_block(x, filters = 16, kernel_size = 3)
-x <- simple_res_block(x, filters = 16, kernel_size = 3)
-
-# Final layers
-x <- x %>%
+  layer_batch_normalization() %>%
+  simple_res_block(filters = 16, kernel_size = 3) %>%
+  simple_res_block(filters = 16, kernel_size = 3) %>%
+  simple_res_block(filters = 16, kernel_size = 3) %>%
+  simple_res_block(filters = 16, kernel_size = 3) %>%
   layer_max_pooling_1d(pool_size = 2) %>%
-  layer_flatten() %>%
+  layer_flatten()
+
+# Combine ResNet output with metadata
+combined <- layer_concatenate(list(x, input_meta)) %>%
+  layer_dense(units = 64, activation = 'relu') %>%
+  layer_dropout(rate = 0.5) %>%
+  layer_dense(units = 32, activation = 'relu') %>%
+  layer_dropout(rate = 0.5) %>%
+  layer_dense(units = 16, activation = 'relu')
+
+output <- combined %>%
   layer_dense(units = 2, activation = 'sigmoid')
 
-# Create the model
-model <- keras_model(inputs = input, outputs = x)
+model <- keras_model(inputs = list(input_cnn, input_meta), outputs = output)
 
-# Compile the model
 model %>% compile(
   loss = loss_categorical_crossentropy,
   optimizer = optimizer_adam(),
   metrics = c('accuracy')
 )
 
-# Model summary
-summary(model)
-
-#---------------------------------------------------
-
-# Compile model
-model %>% compile(
-  loss = loss_categorical_crossentropy,
-  optimizer = optimizer_adam(),
-  metrics = c('accuracy')
-)
-
+# Fit model with metadata
 cnn_history <- model %>% fit(
-  x_train_S, dummy_y_train_S,
+  list(x_train, metadata_train), dummy_y_train,
   batch_size = 1000,
   epochs = 100,
   validation_split = 0.4
 )
 
+# Evaluate and predict with metadata
+evaluate(model, list(x_test, metadata_test), dummy_y_test)
+preds <- predict(model, list(x_test, metadata_test))
 
-evaluate(model, x_test, dummy_y_test)
-preds<-predict(model, x=x_test)
-
-species.predictions<-apply(preds,1,which.max)
-species.predictions<-as.factor(ifelse(species.predictions == 1, "LT", "SMB"))
-confusionMatrix(species.predictions,as.factor(test$species))
+species_predictions <- apply(preds, 1, which.max) - 1
+species_predictions <- ifelse(species_predictions == 0, "LT", "SMB")
+confusionMatrix(factor(species_predictions), factor(test$species))
